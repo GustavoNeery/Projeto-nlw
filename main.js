268dc558338b9ee2caffc1b3552b57ea73d491b0
Luiz Gustavo <gustavoalvesneryy@hotmail.com>
2021-08-25 23:04:53 -0300

    Inserção de novos estilos no about

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+/** criação de evento de click no menu, quando clicar no --- abre quando clicar no x fecha */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
+  })
+}
+
+/** Quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
